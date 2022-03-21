1e4e31a5128e8f33ee2d5556eccc4528cd302983
Rupesh Gudmalwar <rupeshgudmalwar1@gmail.com>
2022-03-21 18:35:24 +0530

    bug fixes

--- src/components/ExportToCSV/export-to-csv.tsx
+++ src/components/ExportToCSV/export-to-csv.tsx
@@ -101,6 +101,20 @@ const ExportToCSV: React.FunctionComponent<any> = () => {
       return sortedRows;
     });
 
+    sortedData = sortedData.map((item) => {
+      let arr = Object.entries(item);
+
+      arr.forEach((itm: any) => {
+        if (typeof itm[1] === 'object') {
+          itm[0] = `${itm[0]}_id`;
+          itm[1] = itm[1]?.id;
+        }
+      });
+
+      let fixedObj = Object.fromEntries(arr);
+      return fixedObj;
+    });
+
     if (sortedData) {
       setSortedDataState([...sortedDataState, ...sortedData]);
     }
