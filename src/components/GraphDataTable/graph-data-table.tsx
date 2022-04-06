e4ea6dc01fbdffad77b9ea1c9197cb21d26087fa
Rupesh Gudmalwar <rupeshgudmalwar1@gmail.com>
2022-04-06 16:49:55 +0530

    error handling improvement on download page

--- src/components/GraphDataTable/graph-data-table.tsx
+++ src/components/GraphDataTable/graph-data-table.tsx
@@ -333,7 +333,7 @@ const GraphDataTable: React.FunctionComponent<GraphDataTableProps & RouteCompone
             label.EMPTY
           )}
 
-          {(rows.length > 0 || error) && !parsed.v ? (
+          {rows.length > 0 || error ? (
             label.EMPTY
           ) : (
             <div className="no-record-found">
