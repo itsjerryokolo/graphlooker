a8ad6b814dbb4fa28b34e0dea1230268275634ff
Sumit Mishra <sumit894sumit@gmail.com>
2022-07-25 16:32:59 +0530

    Updated the Email Body

--- src/components/ErrorMessage/error-message.tsx
+++ src/components/ErrorMessage/error-message.tsx
@@ -3,11 +3,13 @@ import { ErrorMassageProps } from '../../utility/interface/props';
 import './error-message.scss';
 import { customMessages } from '../../utility/utility';
 import Constants from '../../utility/constant';
-import { Button} from '@mui/material';
+import { Button } from '@mui/material';
 import MailIcon from '@mui/icons-material/Mail';
+
 
 const errorLabels = Constants.LABELS.errorComponenet;
 const mailAddress = Constants.CONTACT.EMAIL;
+const URL=window.location.href;
 const ErrorMessage: React.FunctionComponent<ErrorMassageProps> = ({
   errorMessage,
   endpoint,
@@ -16,14 +18,14 @@ const ErrorMessage: React.FunctionComponent<ErrorMassageProps> = ({
   let customMessage: string = customMessages(errorMessage, endpoint);
 
   function getBodyForMail() {
-    return `${Constants.ERROR_MESSAGES.MAIL_BODY} ${window.location.href}`;
-  }
+    return `${Constants.MAIL_FORMAT.MAIL_GREETING} %0D%0D ${Constants.MAIL_FORMAT.MAIL_BODY} %0D%0D ${encodeURIComponent(URL)} %0D%0D ${Constants.MAIL_FORMAT.MAIL_ENDING_MESSAGE}`;
+ }
   function getSubjectForMail() {
-    return `${Constants.ERROR_MESSAGES.MAIL_SUBJECT}`;
+    return `${Constants.MAIL_FORMAT.MAIL_SUBJECT}`;
   }
-  
+        
   return (
-    <>
+    <>                                               
       {type === 'message' ? (
         <div className="error-message">
           <span>{customMessage}</span>
@@ -33,7 +35,18 @@ const ErrorMessage: React.FunctionComponent<ErrorMassageProps> = ({
           <img className="icon" src="/images/error_icon.gif" alt="" />
           <span className="message">{errorLabels.queryFailedMsg}</span>
           <Button variant="contained" endIcon={<MailIcon />}>
-            <a id="email-sender" href={'mailto:' + mailAddress + '?subject=' + getSubjectForMail() + '&body=' + getBodyForMail()} target="_blank">
+            <a
+              id="email-sender"
+              href={
+                'mailto:' +
+                mailAddress +
+                '?subject=' +
+                getSubjectForMail() +
+                '&body=' +
+                getBodyForMail()
+              }
+              target="_blank"
+            >
               {Constants.LABELS.commonLables.BUTTON_TEXT_FOR_EMAIL}
             </a>
           </Button>
